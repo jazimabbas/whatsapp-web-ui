e7460b623d36217d8f1f2350d69ee5107b633b97
Jazim Abbas <jazimabbas87@gmail.com>
2023-03-06 09:24:55 +0500

    added routes setup using react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import ChatPage from "pages/chat";
-import SplashScreen from "pages/splash";
+import React from "react";
 import useAppLoad from "pages/splash/hooks/useAppLoad";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+const ChatPage = React.lazy(() => import("pages/chat"));
+const SplashPage = React.lazy(() => import("pages/splash"));
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<ChatPage />} />)
+);
 
 export default function App() {
   const { isLoaded, progress } = useAppLoad();
 
-  if (!isLoaded) return <SplashScreen progress={progress} />;
-  return <ChatPage />;
+  if (!isLoaded) return <SplashPage progress={progress} />;
+  return <RouterProvider router={router} />;
 }
